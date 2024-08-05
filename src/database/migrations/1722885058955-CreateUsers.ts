2a78d56e13a14c696145b3fdf8c597837c354f32
Luciano Cordeiro <lacs@petrobras.com.br>
2024-08-05 16:46:55 -0300

    Creating migration and model for Users

--- src/database/migrations/1722885058955-CreateUsers.ts
+++ src/database/migrations/1722885058955-CreateUsers.ts
@@ -1,11 +1,11 @@
-import {MigrationInterface, QueryRunner, Table } from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAppointments1721394708034 implements MigrationInterface {
+export default class CreateUsers1722885058955 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-            name: 'appointments',
+            name: 'users',
             columns: [
                 {
                     name: 'id',
@@ -15,12 +15,17 @@ export class CreateAppointments1721394708034 implements MigrationInterface {
                     default: 'uuid_generate_v4()',
                 },
                 {
-                    name: 'provider',
+                    name: 'name',
                     type: 'varchar',
                 },
                 {
-                    name: 'date',
-                    type: 'timestamp with time zone',
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true,
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
                 },
                 {
                     name: 'created_at',
@@ -38,8 +43,9 @@ export class CreateAppointments1721394708034 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
+
 
 
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,26 @@
 
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider: string;
+    name: string;
 
-    @Column('timestamp with time zone')
-    date: Date;
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
 
     @CreateDateColumn()
     created_at: Date;
 
     @UpdateDateColumn()
     updated_at: Date;
-
 }
 
-export default Appointment;
+export default User;
