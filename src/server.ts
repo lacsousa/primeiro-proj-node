e3b76cbf9f32b7a0d30ac60d3572771ee5f45986
Luciano Cordeiro <lacs@petrobras.com.br>
2024-08-29 11:59:31 -0300

    Creating a route to display avatar image files

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 /*
@@ -28,6 +29,9 @@ import './database';
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.get('/', (request, response) => {
